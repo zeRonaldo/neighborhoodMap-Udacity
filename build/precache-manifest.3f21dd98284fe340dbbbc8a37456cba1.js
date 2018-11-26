10839427cd393578704e85e398334e45ab59d3a2
José Ronaldo Souza Junior <ronaldo.souza@faciltecnologia.com.br>
2018-11-26 14:15:23 -0300

    move build

---
+++ build/precache-manifest.3f21dd98284fe340dbbbc8a37456cba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "191e8f310fee558c790f",
+    "url": "/static/css/main.18d671dd.chunk.css"
+  },
+  {
+    "revision": "191e8f310fee558c790f",
+    "url": "/static/js/main.191e8f31.chunk.js"
+  },
+  {
+    "revision": "2a6cab66998e542fb457",
+    "url": "/static/js/1.2a6cab66.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69bd590ce47c5a5d33ec19a68ec8e073",
+    "url": "/static/media/logo.69bd590c.png"
+  },
+  {
+    "revision": "802999851cbe526d2f4370f8a1ef38dc",
+    "url": "/index.html"
+  }
+];
