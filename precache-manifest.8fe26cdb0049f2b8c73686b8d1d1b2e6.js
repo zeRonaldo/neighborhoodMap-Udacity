9cd169f8f8ad2f1abe640c6ca73a33d5aa9c30e8
José Ronaldo Souza Junior <joseronaldosjr@gmail.com>
2018-11-26 23:27:29 -0300

    Updates

---
+++ precache-manifest.8fe26cdb0049f2b8c73686b8d1d1b2e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bd590ce47c5a5d33ec19a68ec8e073",
+    "url": "/neighborhoodMap-Udacity/static/media/logo.69bd590c.png"
+  },
+  {
+    "revision": "108e30e3f3627fc77d1d",
+    "url": "/neighborhoodMap-Udacity/static/js/runtime~main.108e30e3.js"
+  },
+  {
+    "revision": "253bf13db5e7c915d3de",
+    "url": "/neighborhoodMap-Udacity/static/js/main.253bf13d.chunk.js"
+  },
+  {
+    "revision": "66e8d1c949511885c23f",
+    "url": "/neighborhoodMap-Udacity/static/js/1.66e8d1c9.chunk.js"
+  },
+  {
+    "revision": "253bf13db5e7c915d3de",
+    "url": "/neighborhoodMap-Udacity/static/css/main.aba6823c.chunk.css"
+  },
+  {
+    "revision": "eec6dfe32629749802dac37a3f471f25",
+    "url": "/neighborhoodMap-Udacity/index.html"
+  }
+];
